a1d00f4b6e7e967747b9d8f405af88b3f136e020
kate115 <75540720+kate115@users.noreply.github.com>
2021-12-16 22:34:57 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Introduction() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, friend. I'm Star! Don't you think it's
+          lonely out here in space? I want to visit 
+          you some day! Imagine a place where we could
+          live together in peace! That would be the best!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Not Learn React
-        </a>
+        
       </header>
     </div>
   );
 }
 
-export default App;
+export default Introduction;
